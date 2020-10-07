b9b6ddff8bd7bbd7d601275ecab211cc25e217ff
jose moreno <jrdeavila@unicesar.edu.co>
2020-10-07 13:23:32 -0500

    Se confirma punto 2, corrijo la parte de Presentacion, porque pensaba que no era necesario hasta tener todos los puntos hechos. Para asi tener mas facilidad al realizar la interfaz de usuario.

---
+++ Presentacion/GestionLiquidacionAfiliado.cs
@@ -0,0 +1,99 @@
+﻿using BLL;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion
+{
+    class GestionLiquidacionAfiliado
+    {
+        public SubcidiadoServices SubcidiadoServices { get; set; }
+        public ContributivoServices ContributivoServices { get; set; }
+
+        public GestionLiquidacionAfiliado()
+        {
+            SubcidiadoServices = new SubcidiadoServices();
+            ContributivoServices = new ContributivoServices();
+        }
+
+        public double isNumber(string Cadena)
+        {
+            double value = 0;
+            try
+            {
+                Console.Write(Cadena); value = double.Parse(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("El valor ingresado no es numerico");
+                value = isNumber(Cadena);
+            }
+            return value;
+        }
+        public int MostrarItems()
+        {
+            Console.WriteLine("\t\tOpciones\n");
+            Console.WriteLine("Guardar.............(1)");
+            Console.WriteLine("Consultar..........(2)");
+            Console.WriteLine("Eliminar...........(3)");
+            Console.WriteLine("Modificar..........(4)\n");
+            Console.WriteLine("Salir..............(0)\n");
+            return (int)isNumber("Selecione: ");
+        }
+
+        public void TodasLasOpciones(int item)
+        {
+            switch (item)
+            {
+                case 0: { break; }
+                case 1: { GuardarInformacion(DiligenciarInformacion()); new GestionLiquidacionAfiliado(); break; }
+                case 2: { new GestionLiquidacionAfiliado(); break; }
+                case 3: { new GestionLiquidacionAfiliado(); break; }
+                case 4: { new GestionLiquidacionAfiliado(); break; }
+            }
+        }
+
+        public Afiliado DiligenciarAfiliado()
+        {
+            string Nombre, Sexo, Afiliacion = "S";
+            long Cedula;
+            int Edad;
+            Console.WriteLine("Informacion del afiliado\n");
+            Console.Write("Nombre: "); Nombre = Console.ReadLine();
+            Cedula = (long)isNumber("Cedula: ");
+            Edad = (int)isNumber("Edad: ");
+            Console.Write("Sexo"); Sexo = Console.ReadLine();
+            Console.WriteLine("\tAfiliacion");
+            Console.Write("(C) Contributivo (S) Subcidiado : "); Afiliacion = Console.ReadLine();
+            Afiliacion = Afiliacion.ToUpper();
+            if (Afiliacion.Equals("S")) { Afiliacion = "Subcidiado"; return new Afiliado(Nombre, Cedula, Sexo, Edad, Afiliacion); }
+            else if (Afiliacion.Equals("C")) { Afiliacion = "Contributivo"; return new Afiliado(Nombre, Cedula, Sexo, Edad, Afiliacion); }
+            else return DiligenciarAfiliado();
+        }
+        public LiquidacionAfiliado DiligenciarInformacion()
+        {
+            int DiasAfiliacion;            
+            Afiliado NuevoAfiliado = DiligenciarAfiliado();
+            DiasAfiliacion = (int)isNumber("Dias de afiliacion: ");
+            if (NuevoAfiliado.Afiliacion.Equals("Subcidiado")) return new LiquidacionSubcidiado(NuevoAfiliado, DiasAfiliacion);
+            else return new LiquidacionContributivo(NuevoAfiliado, DiasAfiliacion);
+        }
+
+        public void GuardarInformacion(LiquidacionAfiliado LiquidacionAGuardar)
+        {
+            string OP = "S";
+            Console.Write("Desea guardar esta informacion S/N: "); OP = Console.ReadLine();
+            OP = OP.ToUpper();
+            if (OP.Equals("S"))
+            {
+                if (LiquidacionAGuardar is LiquidacionContributivo) ContributivoServices.GuardarLiquidacionContributivo((LiquidacionContributivo)LiquidacionAGuardar);
+                else SubcidiadoServices.GuardarLiquidacionSubcidiado((LiquidacionSubcidiado)LiquidacionAGuardar);
+            }
+            else Console.WriteLine("La informacion no se guardo...");
+        }
+        
+    }
+}
--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -13,26 +13,8 @@ namespace Presentacion
     {
         static void Main(string[] args)
         {
-            Afiliado afiliado = new Afiliado("Jose Ricardo", 1003316620, "Masculino", 19, "Contributivo");
-            Afiliado afiliadoS = new Afiliado("Jose Ricardo", 1003316620, "Masculino", 19, "Subcidiado");
-            LiquidacionAfiliado liquidacion = new LiquidacionContributivo(afiliado, 29);
-            LiquidacionAfiliado liquidacionS = new LiquidacionSubcidiado(afiliadoS, 29);
-            Console.WriteLine(liquidacionS.LiquidacionAficiliacion);
-            Console.WriteLine(liquidacion.LiquidacionAficiliacion);
-            SubcidiadoServices servicesS = new SubcidiadoServices();
-            ContributivoServices servicesC = new ContributivoServices();
-            //servicesS.GuardarLiquidacionSubcidiado((LiquidacionSubcidiado)liquidacionS);
-            servicesC.GuardarLiquidacionContributivo((LiquidacionContributivo)liquidacion);
-            foreach (LiquidacionContributivo i in servicesC.GetTodasLasLiquidacionContributivo())
-            {
-                Console.WriteLine(i.LiquidacionAficiliacion);
-                Console.WriteLine(i.Afiliado.Nombre);
-                Console.WriteLine(i.NumeroLiquidacion);
-            }
-
-            liquidacion = servicesC.BuscarPorNumeroDeLiquidacion(long.Parse(Console.ReadLine()));
-            Console.WriteLine(liquidacion.Afiliado.Cedula);
-            Console.ReadKey();
+            GestionLiquidacionAfiliado Gestion = new GestionLiquidacionAfiliado();
+            Gestion.TodasLasOpciones(Gestion.MostrarItems());
         }
     }
 }
